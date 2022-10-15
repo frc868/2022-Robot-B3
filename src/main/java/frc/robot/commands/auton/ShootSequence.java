3b64aaaaa5cfd6ee82e0f16b7c2a19d063345b52
superpenguin612 <74030080+superpenguin612@users.noreply.github.com>
2022-10-15 07:37:16 -0400

    Drop wait time on shoot sequence, run off of limelight regression

--- src/main/java/frc/robot/commands/auton/ShootSequence.java
+++ src/main/java/frc/robot/commands/auton/ShootSequence.java
@@ -5,7 +5,8 @@ import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
-import frc.robot.commands.RunShooterSetSpeed;
+import frc.robot.commands.RunShooterLockedSpeed;
+import frc.robot.commands.TurnToGoal;
 import frc.robot.sensors.Limelight;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Hopper;
@@ -21,14 +22,15 @@ public class ShootSequence extends SequentialCommandGroup {
     public ShootSequence(Drivetrain drivetrain, Shooter shooter, Limelight limelight, Hopper hopper) {
         addCommands(
                 new InstantCommand(limelight::setVisionMode),
-                // new TurnToGoal(drivetrain, limelight).withTimeout(1.0),
+                new TurnToGoal(drivetrain, limelight).withTimeout(1.5),
                 new ParallelRaceGroup(
-                        new RunShooterSetSpeed(33.0, shooter),
+                        // new RunShooterSetSpeed(33.0, shooter),
+                        new RunShooterLockedSpeed(shooter, limelight),
                         new SequentialCommandGroup(
                                 new WaitUntilCommand(shooter::atSetpoint),
-                                new InstantCommand(hopper::gatekeepersIn, hopper),
+                                new InstantCommand(hopper::gatekeepersOpen, hopper),
                                 new StartEndCommand(hopper::runMotor, hopper::stopMotor, hopper).withTimeout(1.4),
-                                new InstantCommand(hopper::gatekeepersOut, hopper))),
+                                new InstantCommand(hopper::gatekeepersClosed, hopper))),
                 new InstantCommand(limelight::setDriverAssistMode));
 
     }
